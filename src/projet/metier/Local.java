5dcac320a705962baa9fdb8c5a242ce9595fca29
neon1996 <greg.jacq@hotmail.com>
2019-04-11 15:20:31 +0200

    Sauvegarde #6 API :
    Test unit local complet opé.

--- src/projet/metier/Local.java
+++ src/projet/metier/Local.java
@@ -1,6 +1,8 @@
 
 package projet.metier;
 
+import java.util.Objects;
+
 public class Local {
     
     /*Classe métier correspondant à la table local dans la BDD */
@@ -57,6 +59,30 @@ public class Local {
     @Override
     public String toString() {
         return "Local{" + "idlocal=" + idlocal + ", sigle=" + sigle + ", places=" + places + ", description=" + description + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Local other = (Local) obj;
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
+        return true;
     }
     
     
--- test/projet/DAO/LocalDAOTest.java
+++ test/projet/DAO/LocalDAOTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package projet.DAO;
 
 import java.sql.Connection;
@@ -17,10 +12,6 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import projet.metier.Local;
 
-/**
- *
- * @author Florence
- */
 public class LocalDAOTest {
 
     static Connection dbConnect;
@@ -32,7 +23,7 @@ public class LocalDAOTest {
     public static void setUpClass() {
         dbConnect = DBConnection.getConnection();
         if (dbConnect == null) {
-            System.out.println("Connexion invalide");
+            System.out.println("connexion invalide");
             System.exit(1);
         }
     }
@@ -56,13 +47,14 @@ public class LocalDAOTest {
     @Test
     public void testCreate() throws Exception {
         System.out.println("create");
-
-        Local obj = new Local(0, "TestSigle", "TestPlaces", "TestDescription");
+        Local obj = new Local(0, "TestSigle", "5", "TestDescription");
         LocalDAO instance = new LocalDAO();
+        
         instance.setConnection(dbConnect);
-        Local expResult = new Local(0, "TestSigle", "TestPlaces", "TestDescription");
+        
+        Local expResult = new Local(0, "TestSigle", "5", "TestDescription");;
         Local result = instance.create(obj);
-
+        
         assertEquals("Sigle différents", expResult.getSigle(), result.getSigle());
         assertEquals("Places différents", expResult.getPlaces(), result.getPlaces());
         assertEquals("Description différents", expResult.getDescription(), result.getDescription());
@@ -70,43 +62,37 @@ public class LocalDAOTest {
         assertNotEquals("id non généré", expResult.getIdlocal(), result.getIdlocal());
         int idlocal = result.getIdlocal();
 
-        obj = new Local(0, "TestSigle", "TestPlaces", "TestDescription");
+        obj = new Local(0, "TestSigle", "5", "TestDescription");
 
         try {
             obj = instance.create(obj);
             fail("exception de doublon non générée");
             instance.delete(obj);
 
-        } catch (SQLException e) {
-        }
+        } catch (SQLException e) {}
 
         instance.delete(result);
     }
 
     /**
      * Test of readSigle method, of class LocalDAO.
-     *
-     * @throws java.lang.Exception
      */
     @Test
     public void testReadSigle() throws Exception {
         System.out.println("readSigle");
         String sigle = "";
-
         LocalDAO instance = new LocalDAO();
         instance.setConnection(dbConnect);
-        Local obj = new Local(0, "TestSigle", "TestPlaces", "TestDescription");
+        Local obj = new Local(0, "TestSigle", "10", "TestDescription");
         Local expResult = instance.create(obj);
         sigle = expResult.getSigle();
         Local result = instance.readSigle(sigle);
-        //  assertEquals("Sigle différents", expResult.getSigle(), result.getSigle());
-        assertEquals("Places différents", expResult.getPlaces(), result.getPlaces());
-        assertEquals("Description différents", expResult.getDescription(), result.getDescription());
 
-        assertEquals("Sigle différents", expResult.getSigle(), result.getSigle());
+        assertEquals("Sigles differents", expResult.getSigle(), result.getSigle());
+
         try {
             result = instance.read(0);
-            fail("exception de sigle inconnu non générée");
+            fail("Exception d'un code inconnu non genere");
         } catch (SQLException e) {
         }
         instance.delete(result);
@@ -114,24 +100,22 @@ public class LocalDAOTest {
 
     /**
      * Test of read method, of class LocalDAO.
-     *
-     * @throws java.lang.Exception
      */
     @Test
     public void testRead() throws Exception {
-
         System.out.println("read");
+
         int idlocal = 0;
         LocalDAO instance = new LocalDAO();
         instance.setConnection(dbConnect);
-        Local obj = new Local(0, "TestSigle", "TestPlaces", "TestDescription");
+        Local obj = new Local(0, "TestSigle", "10", "TestDescription");
         Local expResult = instance.create(obj);
         idlocal = expResult.getIdlocal();
         Local result = instance.read(idlocal);
         assertEquals("Sigle différents", expResult.getSigle(), result.getSigle());
         assertEquals("Places différents", expResult.getPlaces(), result.getPlaces());
         assertEquals("Description différents", expResult.getDescription(), result.getDescription());
-
+        //etc
         assertEquals("id différents", expResult.getIdlocal(), result.getIdlocal());
         try {
             result = instance.read(0); //0 est la seule valeur dans la bdd qui n'est pas donné dans la bdd car l'auto incrément commence à 1
@@ -143,37 +127,30 @@ public class LocalDAOTest {
 
     /**
      * Test of update method, of class LocalDAO.
-     *
-     * @throws java.lang.Exception
      */
     @Test
     public void testUpdate() throws Exception {
         System.out.println("update");
-
-        Local obj = new Local(0, "TestSigle", "TestPlaces", "TestDescription");
+        Local obj = new Local(0, "TestSigle", "10", "TestDescription");
         LocalDAO instance = new LocalDAO();
         instance.setConnection(dbConnect);
         obj = instance.create(obj);
-        obj.setSigle("TestSigle2");
+        obj.setSigle("TestSigle");
 
         Local expResult = obj;
         Local result = instance.update(obj);
         assertEquals(expResult.getSigle(), result.getSigle());
 
         instance.delete(obj);
-
     }
 
     /**
      * Test of delete method, of class LocalDAO.
-     *
-     * @throws java.lang.Exception
      */
     @Test
     public void testDelete() throws Exception {
         System.out.println("delete");
-
-        Local obj = new Local(0, "TestSigle", "TestPlaces", "TestDescription");
+        Local obj = new Local(0, "TestSigle", "10", "TestDescription");
         LocalDAO instance = new LocalDAO();
         instance.setConnection(dbConnect);
         obj = instance.create(obj);
@@ -183,23 +160,22 @@ public class LocalDAOTest {
             fail("exception de record introuvable non générée");
         } catch (SQLException e) {
         }
-
+        
     }
 
     /**
      * Test of rechDescription method, of class LocalDAO.
-     *
-     * @throws java.lang.Exception
      */
     @Test
+
     public void testRechDescription() throws Exception {
         System.out.println("rechDescription");
-        String descriptionrech = "";
+        Local obj1 = new Local(0, "TestSig", "10", "testdescription");
+        Local obj2 = new Local(0, "TestSig2", "20", "testdescription");
 
-        Local obj1 = new Local(0, "TestSigle", "TestPlaces", "TestDescription");
-        Local obj2 = new Local(0, "TestSigle2", "TestPlaces2", "TestDescription2");
-        String nomrech = "TestSigle";
+        String descriptionrech = "testdescription";
         LocalDAO instance = new LocalDAO();
+
         instance.setConnection(dbConnect);
         obj1 = instance.create(obj1);
         obj2 = instance.create(obj2);
@@ -209,10 +185,9 @@ public class LocalDAOTest {
             fail("record introuvable " + obj1);
         }
         if (result.indexOf(obj2) < 0) {
-            fail("record introuvable" + obj2);
+            fail("record introuvable " + obj2);
         }
         instance.delete(obj1);
         instance.delete(obj2);
     }
-
 }
