a6faf30eeacf88bc753fd87cd1154e69d7bc4789
neon1996 <greg.jacq@hotmail.com>
2019-05-15 14:10:18 +0200

    API : correction pattern observer : correction variable (main).

--- src/design_patterns/observer/Cours.java
+++ src/design_patterns/observer/Cours.java
@@ -2,5 +2,75 @@
 package design_patterns.observer;
 
 public class Cours {
+    protected int idcours;
+    protected String matiere;
+    protected int heures;
+
+    public Cours() {
+    }
+
+    public Cours(int idcours, String matiere, int heures) {
+        this.idcours = idcours;
+        this.matiere = matiere;
+        this.heures = heures;
+    }
+
+    public int getIdcours() {
+        return idcours;
+    }
+
+    public void setIdcours(int idcours) {
+        this.idcours = idcours;
+    }
+
+    public String getMatiere() {
+        return matiere;
+    }
+
+    public void setMatiere(String matiere) {
+        this.matiere = matiere;
+    }
+
+    public int getHeures() {
+        return heures;
+    }
+
+    public void setHeures(int heures) {
+        this.heures = heures;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 89 * hash + this.idcours;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cours other = (Cours) obj;
+        if (this.idcours != other.idcours) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Cours{" + "idcours=" + idcours + ", matiere=" + matiere + ", heures=" + heures + '}';
+    }
+    
+    
+    
+            
     
 }
--- src/design_patterns/observer/Gestion.java
+++ src/design_patterns/observer/Gestion.java
@@ -6,8 +6,8 @@ public class Gestion {
     
     public static void main(String[] args) {
         
-        Local l1 = new Local(1,"LOC1",25,"Description local1");
-        Local l2 = new Local (2,"LOC2",30,"Description local2");
+        Local l1 = new Local(1,"LOC001",25,"Description local1");
+        Local l2 = new Local (2,"LOC001",30,"Description local2");
          
         Formateur f1= new Formateur(1,"Form1","Form1","Form1","16","rue form1","fr1",7156,"4652457");
         Formateur f2= new Formateur(2,"Form2","Form2","Form2","69","rue form2","fr2",7000,"5643145");
@@ -16,8 +16,8 @@ public class Gestion {
          l1.addObserver(f2);
          l2.addObserver(f1);
          
-         l1.setSigle("LOC1");
-         l2.setSigle("LOC2");
+         l1.setSigle("LOC100");
+         l2.setSigle("LOC200");
     }
     
 }
